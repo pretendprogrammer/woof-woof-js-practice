8af56e66ff73bd40d35473b71239a7321aae0e08
Yosef <shmuli5770@gmail.com>
2021-04-15 17:16:35 -0400

    It's actually updating all three locations now and working.

--- src/index.js
+++ src/index.js
@@ -4,45 +4,41 @@ const dogNameH2 = document.querySelector('#dog-name')
 const dogImage = document.querySelector('#dog-image')
 const dogButton = document.querySelector('#dog-button')
 
+let masterObject = {}
+let displayedDog = {}
+
 fetch(`${URL}`)
     .then(response => response.json())
     .then(arrayOfDogPojos => {
         arrayOfDogPojos.forEach(element => {
+            masterObject[element.id] = element
             let newSpan = document.createElement('span')
                 newSpan.append(element.name)
-                newSpan.id = element.id
-                newSpan.addEventListener('click', () => {bringDogToMainView(element)})
+                newSpan.addEventListener('click', () => {bringDogToMainView(element.id)})
             dogBarOfNames.append(newSpan)
         });
     })
 
-function bringDogToMainView(dogObject) {
-    dogImage.src = dogObject.image
-    dogNameH2.textContent = dogObject.name
-    setDogStatus(dogObject.isGoodDog, dogObject.id)
-}
-
-function setDogStatus(currentStatus, currentId) {
-    dogButton.name = currentId
-    dogButton.value = currentStatus
-    let dogStatus
-    currentStatus === true ? dogStatus = 'Good Dog!' : dogStatus = 'Bad Dog!'
-    dogButton.textContent = dogStatus
+function bringDogToMainView(dogId) {
+    displayedDog = masterObject[dogId]
+    dogImage.src = displayedDog.image
+    dogNameH2.textContent = displayedDog.name
+    displayedDog.isGoodDog ? dogButton.textContent = 'Good Dog!' : dogButton.textContent = 'Bad Dog!'
 }
 
 dogButton.addEventListener('click', () => {
-    idOfObjectToUpdate = dogButton.name
-    dogButton.value === 'true' ? newDogStatus = false : newDogStatus = true
-    patchConfig= {
+    displayedDog.isGoodDog ? newDogStatus = false : newDogStatus = true
+    patchConfig = {
         method: "PATCH",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({isGoodDog: newDogStatus})
     }
-    fetch(`${URL}/${idOfObjectToUpdate}`, patchConfig)
+    fetch(`${URL}/${displayedDog.id}`, patchConfig)
         .then(response => response.json())
         .then((updatedDogObject) => {
-            setDogStatus(updatedDogObject.isGoodDog, updatedDogObject.id)
+            masterObject[displayedDog.id] = updatedDogObject
+            bringDogToMainView(displayedDog.id)
         })
 })
